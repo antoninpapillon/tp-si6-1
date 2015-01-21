ead9c2629aedcd907789aa673159289c15cae1a2
Antonin Papillon <antonin.papillon@bts-malraux.net>
2015-01-21 12:04:56 +0100

    Premier truc

---
+++ main.c
@@ -0,0 +1,11 @@
+double resultat, operaudA=14, operaudB=7;
+
+resultat = operaudA * operaudB;
+
+printf("Le résultat de la multiplication de 14 par 7 est : %d ", resultat);
+
+resultat = operaudA / operaudB;
+
+printf("Le résultat de la division de 14 par 7 est : %d", resultat);
+
+
